94ef79b9093309ecf2ca117dc5266c2a3528e480
Thomas Toye <thomas@toye.io>
2018-07-17 17:53:00 +0200

    Implement authorizer

--- common/src/authorize.ts
+++ common/src/authorize.ts
@@ -1,31 +1,50 @@
-// TODO use TypeScript
+import { Callback, Context, CustomAuthorizerEvent, Handler } from 'aws-lambda';
+import * as jwt from 'jsonwebtoken';
+import { buildIAMPolicy } from './build-iam-policy-document';
+
+const getApiOptions = function(event: CustomAuthorizerEvent) {
+  const split = event.methodArn.split(':');
+  const apiGatewayArn = split[5].split('/');
+
+  return {
+    awsAccountId: split[4],
+    region: split[3],
+      restApiId: apiGatewayArn[0],
+      stageName: apiGatewayArn[1],
+  };
+};
 
 /**
  * Authorizer functions are executed before your actual functions.
- * @method authorize
  * @param {String} event.authorizationToken - JWT
  * @throws Returns 401 if the token is invalid or has expired.
  * @throws Returns 403 if the token does not have sufficient permissions.
  */
-export const authorizeHandler = (event, context, callback) => {
-    const token = event.authorizationToken;
+export const authorizeHandler: Handler = (event: CustomAuthorizerEvent, context: Context, callback: Callback) => {
+    const pemKey = process.env.AUTH0_PEM_KEY;
+    const audience = process.env.AUTH0_AUDIENCE;
 
-    const policy = {
-        principalId: 'user-id-here',
-        policyDocument: {
-            Version: '2012-10-17',
-            Statement: [
-                {
-                    Action: 'execute-api:Invoke',
-                    Effect: 'Allow', // 'Deny' if not allowed
-                    Resource: event.methodArn,
-                },
-            ],
-        },
-        context: {
-            i_guess_i_can_add_whatever_here: true
-        },
-    };
+    if (!pemKey) {
+        throw new Error('AUTH0_PEM_KEY not set')
+    }
 
-    callback(null, policy);
+    if (!audience) {
+        throw new Error('AUTH0_AUDIENCE not set')
+    }
+
+    console.log(`audience: ${audience}, pem key: ${pemKey}`);
+
+    console.log('auth event: ', event);
+
+    const token = event.authorizationToken.replace(/Bearer /g, '');
+    const apiOptions = getApiOptions(event);
+
+    jwt.verify(token, pemKey, { audience }, (err, verified: any) => {
+        if (err) {
+          console.error('JWT Error', err, err.stack);
+          callback(null, buildIAMPolicy('anonymous', 'Deny', event.methodArn));
+        } else {
+          callback(null, buildIAMPolicy(verified.sub, 'Allow', event.methodArn))
+        }
+    });
 };
---
+++ common/src/build-iam-policy-document.ts
@@ -0,0 +1,27 @@
+/**
+ * Returns an IAM policy document for a given user and resource.
+ *
+ * @method buildIAMPolicy
+ * @param {String} userId - user id
+ * @param {String} effect  - Allow / Deny
+ * @param {String} resource - resource ARN
+ * @param {String} context - response context
+ * @returns {Object} policyDocument
+ */
+
+export const buildIAMPolicy = (userId: string, effect: 'Allow' | 'Deny', resource: string, context: any = {}) => {
+    return {
+        principalId: userId,
+        policyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Action: 'execute-api:Invoke',
+                    Effect: effect,
+                    Resource: resource,
+                },
+            ],
+        },
+        context,
+    };
+};
