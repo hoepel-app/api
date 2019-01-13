7e7a0327f756d445225291c2e350071d2d3c3a69
Thomas Toye <thomas@toye.io>
2019-01-13 22:49:28 +0100

    Revamp user create hook

--- hoepel-app-functions/functions/src/user-create.ts
+++ hoepel-app-functions/functions/src/user-create.ts
@@ -1,44 +1,26 @@
 import * as functions from 'firebase-functions';
+import {nodemailerMailgun} from "./services/mailgun";
+import * as admin from "firebase-admin";
 
-/*
+const db = admin.firestore();
 
-Example user object:
+export const onUserCreate = functions.region('europe-west1').auth.user().onCreate(async user => {
+  await nodemailerMailgun.sendMail({
+    from: 'noreply@mail.hoepel.app',
+    to: 'thomas@toye.io',
+    subject: `Nieuwe persoon geregistreerd: ${user.displayName || user.email}`,
+    replyTo: 'help@hoepel.app',
+    text: `Een nieuwe persoon heeft zich geregistreerd. Naam: ${user.displayName || '<geen naam>'}, contact: ${user.email}. Details als bijlage.`,
+    attachments: [ {
+      content: JSON.stringify(user, null, 2),
+      filename: 'user-record.txt',
+    } ],
+  });
 
-{ email: 'thomas@toye.io',
-  emailVerified: false,
-  displayName: 'Thomas Toye',
-  photoURL: 'https://graph.facebook.com/11111111111111/picture',
-  phoneNumber: null,
-  disabled: false,
-  providerData:
-   [ { displayName: 'Thomas Toye',
-       email: 'thomas@toye.io',
-       photoURL: 'https://graph.facebook.com/1111111111111/picture',
-       providerId: 'facebook.com',
-       uid: '1111111111111111111',
-       toJSON: [Function] } ],
-  customClaims: {},
-  passwordSalt: null,
-  passwordHash: null,
-  tokensValidAfterTime: null,
-  metadata:
-   UserRecordMetadata {
-     creationTime: '2018-10-18T09:37:32Z',
-     lastSignInTime: '2018-10-18T09:37:32Z' },
-  uid: 'vAK0raaaaaaaaaaaaaaaaaaaa',
-  toJSON: [Function] }
+  await db.collection('users').doc(user.uid).set({
+    displayName: user.displayName || null,
+    email: user.email,
+  }, { merge: true });
 
-
-
-  ==> is an UserRecord?
-
-  https://firebase.google.com/docs/reference/functions/functions.auth.UserRecord
-
-*/
-
-export const onUserCreate = functions.region('europe-west1').auth.user().onCreate((user) => {
-  // Create new record in users collection with UID of user and roles for tenants
-  // ...
-
-  console.log('New user created!', user);
+  return {};
 });
