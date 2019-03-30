1cefcbd004fb495a237bafb0fb640f95a083cda1
Thomas Toye <thomas@toye.io>
2019-03-30 19:00:40 +0100

    Implement createChildByParent and updateChildByParent in api function

--- hoepel-app-functions/functions/src/routes/speelpleinwerking.com.routes.ts
+++ hoepel-app-functions/functions/src/routes/speelpleinwerking.com.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import * as admin from "firebase-admin";
 import { Child, IChild, Tenant } from "@hoepel.app/types";
 import { firebaseIsAuthenticatedSpeelpleinwerkingDotComMiddleware } from "../middleware/is-authenticated.middleware";
+import { DocumentSnapshot } from "firebase-functions/lib/providers/firestore";
 
 const db = admin.firestore();
 
@@ -49,4 +50,46 @@ router.get('/organisation/:organisation/children/managed-by/:parentUid', async (
   ).docs.map(snapshot => new Child({ ...snapshot.data() as IChild, id: snapshot.id }));
 
   res.json(children);
+});
+
+router.put('/organisation/:organisation/children/:child', async (req, res) => {
+  const childId = req.params.child;
+  const organisationId = req.params.organisation;
+  const parentUid = res.locals.user.uid;
+
+  const snapshot: DocumentSnapshot = (await db.collection('children').doc(childId).get());
+
+  if (!snapshot.exists) {
+    res.status(404).json({ error: 'Child not found' });
+  } else if (snapshot.data().tenant !== organisationId) {
+    res.status(403).json({ error: `Tenant id on child (${snapshot.data().tenant}) does not match given tenant id (${organisationId})` });
+  } else if ((snapshot.data().managedByParents || [] as ReadonlyArray<string>).indexOf(parentUid) === -1) {
+    res.status(403).json({ error: `Parent can not edit child since parent uid (${parentUid}) is not included in ${JSON.stringify(snapshot.data().managedByParents)}` });
+  } else {
+
+    // First, create a new child object. This way, only valid properties are kept
+    // Then stringify and parse as JSON. We get a plain JS object
+    // Finally, drop the id
+    const { id, ...newChild } = JSON.parse(JSON.stringify(new Child(req.body.child)));
+    const newChildWithTenant = { ...newChild, tenant: organisationId };
+
+    await db.collection('children').doc(childId).set(newChildWithTenant);
+
+    res.sendStatus(200);
+  }
+});
+
+router.post('/organisation/:organisation/children', async (req, res) => {
+  const organisationId = req.params.organisation;
+  const parentUid = res.locals.user.uid;
+
+  // First, create a new child object. This way, only valid properties are kept
+  // Then stringify and parse as JSON. We get a plain JS object
+  // Finally, drop the id
+  const { id, ...newChild } = JSON.parse(JSON.stringify(new Child(req.body.child).withManagedByParents([ parentUid ])));
+  const newChildWithTenant = { ...newChild, tenant: organisationId };
+
+  await db.collection('children').add(newChildWithTenant);
+
+  res.sendStatus(200);
 });
