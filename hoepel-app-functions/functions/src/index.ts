15916fea38485f150a87b25e90058dc1861f1618
Thomas Toye <thomas@toye.io>
2019-04-03 20:10:52 +0200

    Add TODO

--- hoepel-app-functions/functions/src/index.ts
+++ hoepel-app-functions/functions/src/index.ts
@@ -9,6 +9,7 @@ db.settings({timestampsInSnapshots: true});
 export * from './firestore-events/';
 export * from './user-events/';
 
+// TODO these need to be moved to api function
 export * from './file-creation';
 export * from './templates';
 export * from './template-fill-in';
