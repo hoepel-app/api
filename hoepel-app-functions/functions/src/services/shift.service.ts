d11de26f1e95574e63d884a37d78c2fc9fbd5b85
Thomas Toye <thomas@toye.io>
2019-06-07 20:33:47 +0200

    Update dependencies

--- hoepel-app-functions/functions/src/services/shift.service.ts
+++ hoepel-app-functions/functions/src/services/shift.service.ts
@@ -10,12 +10,19 @@ export class ShiftService {
 
   async getMany(tenant: string, shiftIds: ReadonlyArray<string>): Promise<ReadonlyArray<Shift>> {
     // tslint:disable:no-unnecessary-type-assertion
-    const shifts = (await this.db.getAll(...shiftIds.map(id => this.db.collection('shifts').doc(id))))
-      .map(snapshot => ({...snapshot.data(), id: snapshot.id} as IShift & { tenant: string }))
-      .filter(ishift => ishift.tenant === tenant)
-      .map(ishift => new Shift(dropTenant(ishift)));
 
-    return Shift.sort(shifts);
+    if (shiftIds.length > 0) {
+      const id2doc = (id: string) => this.db.collection('shifts').doc(id);
+
+      const shifts = (await this.db.getAll(id2doc(shiftIds[0]), ...shiftIds.slice(1).map(id2doc))) // can't do ...shiftIds.map(id2doc) due to some TS error
+        .map(snapshot => ({...snapshot.data(), id: snapshot.id} as IShift & { tenant: string }))
+        .filter(ishift => ishift.tenant === tenant)
+        .map(ishift => new Shift(dropTenant(ishift)));
+
+      return Shift.sort(shifts);
+    } else {
+      return Promise.resolve([]);
+    }
   }
 
   /**
