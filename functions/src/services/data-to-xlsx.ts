044deedbf11bdc1485a088d2decf8a80f9ce20fe
Thomas Toye <thomas@toye.io>
2019-07-13 21:05:50 +0200

    Use ExcelData interface in all data generation functions
    Use new rich child attendance class

--- functions/src/services/data-to-xlsx.ts
+++ functions/src/services/data-to-xlsx.ts
@@ -4,7 +4,7 @@ import {
   Child,
   ContactPerson,
   Crew,
-  DayDate,
+  DayDate, DetailedChildAttendancesOnShift, DetailedChildAttendancesOnShifts,
   IChild,
   ICrew,
   IDetailedChildAttendance,
@@ -14,9 +14,10 @@ import {
 import { AddressService } from './address.service';
 import * as _ from 'lodash';
 
-// Supported types for Excel cells
+/** Supported types for Excel cells */
 export type ExcelCellValue = string | number | boolean | DayDate | Price;
 
+/** Represents an Excel worksheet (a "tab" in a spreadsheet) */
 export interface ExcelWorksheet {
   name: string;
 
@@ -27,7 +28,7 @@ export interface ExcelWorksheet {
 }
 
 // This interface decouples the results so they don't use SheetJS directly
-// TODO should be used in all functions in this file (and then they should be refactored into classes)
+// TODO should be used in all functions in this file (and then they could be refactored into classes)
 export interface ExcelData {
   filename?: string;
   worksheets: ReadonlyArray<ExcelWorksheet>;
@@ -79,8 +80,6 @@ export const createExcelFile = (data: ExcelData): LocalFileCreationResult => {
     result['!ref'] = XLSX.utils.encode_cell({ c: 0, r: 0 })
       + ':'
       + XLSX.utils.encode_cell({ c: numColumns + 1, r: numRows + 1 });
-
-    console.log(JSON.stringify(result));
 
     return result;
   };
@@ -213,44 +212,44 @@ export const childrenWithCommentsListToXlsx = (list: ReadonlyArray<IChild>, tena
 export const createCrewAttendanceXlsx = (allCrew: ReadonlyArray<Crew>, shifts: ReadonlyArray<Shift>, attendances: ReadonlyArray<{ shiftId: string, attendances: ReadonlyArray<any> }>, year: number, tenant: string): LocalFileCreationResult => {
 
   const sortedShifts = Shift.sort(shifts);
+  const filteredCrew = Crew
+    .sorted(allCrew)
+    .filter(crew => attendances.map(att => !!att.attendances[crew.id]).includes(true));
 
-  const rows = Crew.sorted(allCrew).map(crew => {
-    return {
-      crew,
-      attendances: sortedShifts.map(shift => {
-        const attendanceForShift = attendances.find(att => att.shiftId === shift.id);
-        if (attendanceForShift && attendanceForShift.attendances[crew.id] && attendanceForShift.attendances[crew.id].didAttend) {
-          return 1;
-        } else {
-          return 0;
-        }
-      }),
-    };
-  });
+  const data: ExcelData = {
+    worksheets: [
+      {
+        name: `Aanwezigheden animatoren ${year}`,
+        columns: [
+          { values: [ '', '', 'Voornaam', ...filteredCrew.map(row => row.firstName) ], width: 20 },
+          { values: [ '', '', 'Familienaam', ...filteredCrew.map(row => row.lastName) ], width: 25 },
 
-  const data = [
-    [ '', 'Dag', ...sortedShifts.map(shift => DayDate.fromDayId(shift.dayId).nativeDate) ],
-    [ '', 'Type', ...sortedShifts.map(shift => shift.kind) ],
-    [ 'Voornaam', 'Familienaam', ...sortedShifts.map(shift => shift.description) ],
-    ...rows.map(row => [ row.crew.firstName, row.crew.lastName, ...row.attendances ]),
-  ];
+          ...sortedShifts.map(shift => {
 
-  const workbook = XLSX.utils.book_new();
-
-  const worksheet: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
-  worksheet['!cols'] = [
-    { wch: 20 }, { wch: 25 }, ...sortedShifts.map(ignored => ({ wch: 22 }))
-  ];
-
-  XLSX.utils.book_append_sheet(workbook, worksheet, `Aanwezigheden animatoren ${year}`);
-  const file = XLSX.write(workbook, { bookType: 'xlsx', bookSST: false, type: 'buffer' });
-
-  return {
-    format: 'XLSX',
-    description: `Aanwezigheden animatoren ${year}`,
-    downloadFileName: `Aanwezigheden animatoren ${year}.xlsx`,
-    file,
+            return {
+              values: [
+                DayDate.fromDayId(shift.dayId),
+                shift.kind,
+                shift.description,
+                ...filteredCrew.map(crew => {
+                  const attendanceForShift = attendances.find(att => att.shiftId === shift.id);
+                  if (attendanceForShift && attendanceForShift.attendances[crew.id] && attendanceForShift.attendances[crew.id].didAttend) {
+                    return 1;
+                  } else {
+                    return 0;
+                  }
+                }),
+              ],
+              width: 22,
+            }
+          }),
+        ]
+      }
+    ],
+    filename: `Aanwezigheden animatoren ${year}`,
   };
+
+  return createExcelFile(data);
 };
 
 export const createChildAttendanceXlsx = (
@@ -262,44 +261,45 @@ export const createChildAttendanceXlsx = (
 {
 
   const sortedShifts = Shift.sort(shifts);
+  const filteredChildren = Child
+    .sorted(allChildren)
+    // Only children with attendances
+    .filter(child => attendances.map(att => !!att.attendances[child.id]).includes(true));
 
-  const rows = Child.sorted(allChildren).map(child => {
-    return {
-      child,
-      attendances: sortedShifts.map(shift => {
-        const attendanceForShift = attendances.find(att => att.shiftId === shift.id);
-        if (attendanceForShift && attendanceForShift.attendances[child.id] && attendanceForShift.attendances[child.id].didAttend) {
-          return 1;
-        } else {
-          return 0;
-        }
-      }),
-    };
-  }).filter(row => row.attendances.find(att => att === 1)); // Only children with attendances
+  const data: ExcelData = {
+    worksheets: [
+      {
+        name: `Aanwezigheden kinderen ${year}`,
+        columns: [
+          { values: [ '', '', 'Voornaam', ...filteredChildren.map(row => row.firstName) ], width: 20 },
+          { values: [ '', '', 'Familienaam', ...filteredChildren.map(row => row.lastName) ], width: 25 },
 
-  const data = [
-    [ '', 'Dag', ...sortedShifts.map(shift => DayDate.fromDayId(shift.dayId).nativeDate) ],
-    [ '', 'Type', ...sortedShifts.map(shift => shift.kind) ],
-    [ 'Voornaam', 'Familienaam', ...sortedShifts.map(shift => shift.description) ],
-    ...rows.map(row => [ row.child.firstName, row.child.lastName, ...row.attendances ]),
-  ];
+          ...sortedShifts.map(shift => {
 
-  const workbook = XLSX.utils.book_new();
-
-  const worksheet: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
-  worksheet['!cols'] = [
-    { wch: 20 }, { wch: 25 }, ...sortedShifts.map(ignored => ({ wch: 22 }))
-  ];
-
-  XLSX.utils.book_append_sheet(workbook, worksheet, `Aanwezigheden kinderen ${year}`);
-  const file = XLSX.write(workbook, { bookType: 'xlsx', bookSST: false, type: 'buffer' });
-
-  return {
-    format: 'XLSX',
-    description: `Aanwezigheden kinderen ${year}`,
-    downloadFileName: `Aanwezigheden kinderen ${year}.xlsx`,
-    file,
+            return {
+              values: [
+                DayDate.fromDayId(shift.dayId),
+                shift.kind,
+                shift.description,
+                ...filteredChildren.map(child => {
+                  const attendanceForShift = attendances.find(att => att.shiftId === shift.id);
+                  if (attendanceForShift && attendanceForShift.attendances[child.id] && attendanceForShift.attendances[child.id].didAttend) {
+                    return 1;
+                  } else {
+                    return 0;
+                  }
+                }),
+              ],
+              width: 22,
+            }
+          }),
+        ]
+      }
+    ],
+    filename: `Aanwezigheden kinderen ${year}`,
   };
+
+  return createExcelFile(data);
 };
 
 export const createAllFiscalCertsXlsx = (
@@ -310,127 +310,120 @@ export const createAllFiscalCertsXlsx = (
 ): LocalFileCreationResult => {
 
   const sortedShifts = Shift.sort(shifts);
+  const richAttendances = new DetailedChildAttendancesOnShifts(
+    attendances.map(att => new DetailedChildAttendancesOnShift(att.shiftId, att.attendances))
+  );
+  const sortedChildren = Child.sorted(allChildren).filter(child => richAttendances.numberOfAttendances(child.id) > 0);
 
-  const rows = Child.sorted(allChildren).map(child => {
-    const address = AddressService.getAddressForChildWithExistingContacts(child, allContacts);
+  const spacer = ['', '', ''];
 
-    const primaryContactPerson = child.primaryContactPerson ?
-      allContacts.find(contact => contact.id === child.primaryContactPerson.contactPersonId) || null :
-      null;
+  const data: ExcelData = {
+    filename: `Data fiscale attesten ${year}`,
+    worksheets: [{
+      name: `Data fiscale attesten ${year}`,
+      columns: [
+        {
+          width: 25,
+          values: [...spacer, 'Voornaam', ...sortedChildren.map(child => child.firstName)],
+        },
+        {
+          width: 25,
+          values: [...spacer, 'Familienaam', ...sortedChildren.map(child => child.lastName)],
+        },
+        {
+          width: 25,
+          values: [...spacer, 'Totaal (incl. korting)', ...sortedChildren.map(child => richAttendances.amountPaidBy(child.id))],
+        },
+        {
+          width: 25,
+          values: [...spacer, 'Geboortedatum', ...sortedChildren.map(child => child.birthDate ? new DayDate(child.birthDate) : '')],
+        },
+        {
+          width: 25,
+          values: [...spacer, 'Contactpersoon', ...sortedChildren.map(child => {
+            const primaryContactPerson = child.primaryContactPerson ?
+              allContacts.find(contact => contact.id === child.primaryContactPerson.contactPersonId) || null :
+              null;
 
-    return {
-      child,
-      primaryContactPerson,
-      address: address ? address : new Address({ }),
-      attendances: sortedShifts.map(shift => {
-        const attendanceForShift = attendances.find(att => att.shiftId === shift.id);
-        if (attendanceForShift && attendanceForShift.attendances[child.id] && attendanceForShift.attendances[child.id].didAttend) {
-          return 1;
-        } else {
-          return 0;
-        }
-      }),
-      totalPaid: Price.total(...sortedShifts.map(shift => {
-        const attendanceForShift = attendances.find(att => att.shiftId === shift.id);
-        if (attendanceForShift && attendanceForShift.attendances[child.id] && attendanceForShift.attendances[child.id].didAttend) {
-          return new Price(attendanceForShift.attendances[child.id].amountPaid);
-        } else {
-          return Price.zero;
-        }
-      })),
-    };
-  }).filter(row => row.attendances.find(att => att === 1)); // Only children with attendances
-
-  const data = [
-    [ ...Array(8).fill(''), 'Dag', ...sortedShifts.map(shift => DayDate.fromDayId(shift.dayId).nativeDate) ],
-    [ ...Array(8).fill(''), 'Type', ...sortedShifts.map(shift => shift.kind) ],
-    [ ...Array(8).fill(''), 'Prijs', ...sortedShifts.map(shift => shift.price.toString()) ],
-    [ 'Voornaam', 'Familienaam', 'Totaal (incl. korting)', 'Geboortedatum', 'Contactpersoon', 'Straat en nummer', 'Postcode', 'Stad', '', ...sortedShifts.map(shift => shift.description) ],
-    ...rows.map(row => {
-      const birthDate = row.child.birthDate ? new DayDate(row.child.birthDate).nativeDate : '';
-      return [
-        row.child.firstName,
-        row.child.lastName,
-        row.totalPaid.toString(),
-        birthDate,
-        row.primaryContactPerson ? row.primaryContactPerson.fullName : '',
-        (row.address.street || '') + ' ' + (row.address.number || ''),
-        row.address.zipCode || '',
-        row.address.city || '',
-        '',
-        ...row.attendances
-      ];
-    }),
-  ];
-
-  const workbook = XLSX.utils.book_new();
-
-  const worksheet: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
-  worksheet['!cols'] = [
-    ... Array(9).fill({ wch: 25 }),
-    ...sortedShifts.map(ignored => ({ wch: 22 }))
-  ];
-
-  XLSX.utils.book_append_sheet(workbook, worksheet, `Fiscale attesten ${year}`);
-  const file = XLSX.write(workbook, { bookType: 'xlsx', bookSST: false, type: 'buffer' });
-
-  return {
-    format: 'XLSX',
-    description: `Data fiscale attesten ${year}`,
-    downloadFileName: `Data fiscale attesten ${year}.xlsx`,
-    file,
+            return primaryContactPerson ? primaryContactPerson.fullName : '';
+          })],
+        },
+        {
+          width: 25, values: [...spacer, 'Straat en nummer', ...sortedChildren.map(child => {
+            const address = AddressService.getAddressForChildWithExistingContacts(child, allContacts) || new Address({});
+            return (address.street || '') + ' ' + (address.number || '')
+          })],
+        },
+        {
+          width: 25, values: [...spacer, 'Postcode', ...sortedChildren.map(child => {
+            const address = AddressService.getAddressForChildWithExistingContacts(child, allContacts) || new Address({});
+            return address.zipCode || '';
+          })],
+        },
+        {
+          width: 25, values: [...spacer, 'Stad', ...sortedChildren.map(child => {
+            const address = AddressService.getAddressForChildWithExistingContacts(child, allContacts) || new Address({});
+            return address.city || '';
+          })],
+        },
+        {
+          width: 25, values: ['Dag', 'Type', 'Prijs']
+        },
+        ...sortedShifts.map(shift => {
+          return {
+            width: 22,
+            values: [
+              DayDate.fromDayId(shift.dayId),
+              shift.kind,
+              shift.price,
+              shift.description,
+              ...sortedChildren.map(child => richAttendances.didAttend(child.id, shift.id)),
+            ]
+          }
+        })
+      ],
+    }],
   };
+
+  return createExcelFile(data);
 };
 
 export const createChildrenPerDayXlsx = (
   allChildren: ReadonlyArray<Child>,
   shifts: ReadonlyArray<Shift>,
-  attendances: ReadonlyArray<{ shiftId: string, attendances: { [childId: string]: IDetailedChildAttendance } }>,
+  allAttendances: ReadonlyArray<{ shiftId: string, attendances: { [childId: string]: IDetailedChildAttendance } }>,
   year: number,
   tenant: string
 ): LocalFileCreationResult => {
 
-  // TODO This function should filter out attendances where didAttend === false
+  const detailedAttendances = new DetailedChildAttendancesOnShifts(
+    allAttendances.map(detailed => new DetailedChildAttendancesOnShift(detailed.shiftId, detailed.attendances))
+  );
 
-  const data = _.toPairs(_.groupBy(shifts, shift => shift.dayId))
+  const list = _.toPairs(_.groupBy(shifts, shift => shift.dayId))
     .map(( [dayId, shiftsOnDay] ) => {
-      const attendancesForShiftsOnDay = attendances.filter(att => {
-        return shiftsOnDay.map(shift => shift.id).includes(att.shiftId);
-      });
-
-      const childAttendancesOnDay = attendancesForShiftsOnDay
-        .reduce((a, b) => {
-          return a.concat(_.toPairs(b.attendances).map(pair => ( { ...pair[1], childId: pair[0] })));
-        }, [] as ReadonlyArray<IDetailedChildAttendance & { childId: string }>)
-        .map(att => att.childId);
-
-      const uniqueAttendancesOnDay = [ ...new Set(childAttendancesOnDay) ].length;
+      const uniqueAttendancesOnDay = detailedAttendances.uniqueAttendances(shiftsOnDay.map(shift => shift.id));
 
       return {
         day: DayDate.fromDayId(dayId),
         shifts: shiftsOnDay,
-        attendances: attendancesForShiftsOnDay,
         uniqueAttendancesOnDay,
       }
     })
-    .sort((a, b) => a.day.compareTo(b.day))
-    .map(obj => {
-      return [ obj.day.nativeDate, obj.uniqueAttendancesOnDay ];
-    });
+    .sort((a, b) => a.day.compareTo(b.day));
 
-  const workbook = XLSX.utils.book_new();
-
-  const worksheet: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet([ ['Dag', 'Aantal unieke kinderen'], ...data ]);
-
-  worksheet['!cols'] = [ { wch: 20 }, { wch: 25 } ];
-
-  XLSX.utils.book_append_sheet(workbook, worksheet, `Aantal kinderen per dag ${year}`);
-  const file = XLSX.write(workbook, {bookType: 'xlsx', bookSST: false, type: 'buffer'});
-
-  return {
-    format: 'XLSX',
-    description: `Aantal kinderen per dag ${year}`,
-    downloadFileName: `Aantal unieke kinderen per dag ${year}.xlsx`,
-    file,
+  const data: ExcelData = {
+    worksheets: [
+      {
+        name: `Aantal unieke kinderen ${year}`,
+        columns: [
+          { values: [ 'Dag', ...list.map(row => row.day) ], width: 20 },
+          { values: [ 'Aantal unieke kinderen', ...list.map(row => row.uniqueAttendancesOnDay) ], width: 25 },
+        ]
+      }
+    ],
+    filename: `Aantal unieke kinderen per dag ${year}`,
   };
+
+  return createExcelFile(data);
 };
